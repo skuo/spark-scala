8be719a007498bc8a7c68d7741216163a84b0924
Steve Kuo <steve.kuo@makerstudios.com>
2016-09-07 18:18:30 -0700

    Added TextStream.scala

---
+++ src/main/scala/TextStreaming.scala
@@ -0,0 +1,39 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.StreamingContext
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.Duration
+import org.apache.spark.streaming.Seconds
+import org.apache.log4j.{Level, Logger}
+
+object TextStreaming { 
+   def main(args: Array[String]) { 
+
+       println("Start of TextStreaming")
+       // Create a StreamingContext with a 1-second batch size from a SparkConf
+       val conf = new SparkConf().setMaster("local").setAppName("Text Streaming")
+       val ssc = new StreamingContext(conf,Seconds(15))
+
+       // Set the Logging level to WARN
+       val rootLogger = Logger.getRootLogger()
+       rootLogger.setLevel(Level.WARN)
+
+       // Create a DStream using data received when a new file appears in the monitored directory
+       val lines = ssc.textFileStream("/Users/skuo/data/spark/streaming")
+       println("=============== lines.print() =============")
+       lines.print()
+       val words = lines.flatMap(_.split(" "))
+       val wc = words.map(x => (x, 1)).reduceByKey((x, y) => x + y)
+
+       wc.saveAsTextFiles("/Users/skuo/data/spark/output")
+       wc.print
+
+       // Start our streaming context and wait for it to "finish"
+       println("TextStreaming: sscStart")
+       ssc.start()
+       // Wait for the job to finish
+       println("TextStreaming: awaitTermination")
+       ssc.awaitTermination()
+       println("TextStreaming: done!")
+   } 
+} 
